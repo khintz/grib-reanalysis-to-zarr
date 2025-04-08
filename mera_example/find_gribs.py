506704424b86de08e27182255404173faedda566
Kasper Stener Hintz <kah@dmi.dk>
2025-04-08 12:40:45 +0000

    more work on finding mera gribfiles

--- mera_example/find_gribs.py
+++ mera_example/find_gribs.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from isodate import parse_duration
 from pathlib import Path
+import yaml
 
 def find_files_for_time_interval(t_start: datetime, t_end: datetime) -> list[str]:
 
@@ -9,29 +11,55 @@ def find_files_for_time_interval(t_start: datetime, t_end: datetime) -> list[str
     path_format = "{indicatorofparameter}/{leveltype}/{level}/{timerangeindicator}"
     filename_format = "MERA_PRODYEAR_{start:%Y_%m}_{indicatorofparameter}_{leveltype}_{level}_{timerangeindicator}_{suffix}"
     suffixes = ["ANALYSIS"]
-    
-    indicatorofparameter = "11"
-    leveltype = "105"
-    level = "2"
-    timerangeindicator = "0"
+
+    # list of tuples for each parameter in format indicatorofparameter, leveltype, level, timerangeindicator
+    parameters = [
+        (1, 100, 500, 0),
+        (1, 103, 0, 0),
+        (1, 105, 0, 0),
+        (6, 100, 1000, 0),
+        (6, 100, 500, 0),
+        (11, 100, 500, 0),
+        (11, 100, 850, 0),
+        (11, 105, 2, 0),
+        (11, 105, 30, 0),
+        (33, 100, 850, 0),
+        (33, 105, 30, 0),
+        (34, 100, 850, 0),
+        (34, 105, 30, 0),
+        (52, 105, 2, 0),
+        (52, 105, 30, 0),
+        (54, 200, 0, 0),
+        (61, 105, 0, 4),
+        (113, 8, 0, 4),
+        (115, 105, 0, 4),
+        (116, 105, 0, 4)
+    ]
+
 
     paths = []
     t = t_start
+
     for suffix in suffixes:
 
-        path_file = path_format.format(indicatorofparameter=indicatorofparameter,
-                                        leveltype=leveltype,
-                                        level=level,
-                                        timerangeindicator=timerangeindicator)
+        for parameter in parameters:
 
-        filename = filename_format.format(start=t,
-                                          indicatorofparameter=indicatorofparameter,
-                                          leveltype=leveltype,
-                                          level=level,
-                                          timerangeindicator=timerangeindicator,
-                                          suffix=suffix)
+            indicatorofparameter, leveltype, level, timerangeindicator = parameter
 
-        paths.append(root_path / path_file / filename)
+            path_file = path_format.format(indicatorofparameter=indicatorofparameter,
+                                            leveltype=leveltype,
+                                            level=level,
+                                            timerangeindicator=timerangeindicator)
+
+            filename = filename_format.format(start=t,
+                                            indicatorofparameter=indicatorofparameter,
+                                            leveltype=leveltype,
+                                            level=level,
+                                            timerangeindicator=timerangeindicator,
+                                            suffix=suffix)
+
+            paths.append(root_path / path_file / filename)
 
     print(paths)
+    print(len(paths))
     return paths
